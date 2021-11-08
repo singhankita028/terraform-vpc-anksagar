381450824be05c6ba6866c9bc1f075256419d5c1
anksagar <ankita.a.sagar@capgemini.org>
2021-11-08 12:45:46 -0500

    sec grps

--- secgrps.tf
+++ secgrps.tf
@@ -1,78 +1,78 @@
 resource "aws_security_group" "anksagar-bean-elb-sg" {
-  name = "anksagar-bean-elb-sg"
+  name        = "anksagar-bean-elb-sg"
   description = "Security group for bean-elb"
-  vpc_id = "module.vpc.vpc_id"
+  vpc_id      = module.vpc.vpc_id
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    protocol  = "tcp"
-    to_port   = 80
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 resource "aws_security_group" "anksagar-bastion-sg" {
-  name = "anksagar-bastion-sg"
+  name        = "anksagar-bastion-sg"
   description = "Security group for bastion host ec2 instance"
-  vpc_id = "module.vpc.vpc_id"
+  vpc_id      = module.vpc.vpc_id
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = [var.MYIP]
   }
 }
 resource "aws_security_group" "anksagar-prod-sg" {
-  name = "anksagar-prod-sg"
+  name        = "anksagar-prod-sg"
   description = "Security group for beanstalk instances"
-  vpc_id = "module.vpc.vpc_id"
+  vpc_id      = module.vpc.vpc_id
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
+    from_port       = 22
+    protocol        = "tcp"
+    to_port         = 22
     security_groups = [aws_security_group.anksagar-bastion-sg.id]
   }
 }
 
 resource "aws_security_group" "anksagar-backend-sg" {
-  name = "anksagar-backend-sg"
+  name        = "anksagar-backend-sg"
   description = "Security group for RDS, active mq, elastic cache"
-  vpc_id = "module.vpc.vpc_id"
+  vpc_id      = module.vpc.vpc_id
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port       = 0
+    protocol        = "-1"
+    to_port         = 0
     security_groups = [aws_security_group.anksagar-prod-sg.id]
   }
 }
 resource "aws_security_group_rule" "sec_group_allow_itself" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "tcp"
-  security_group_id = aws_security_group.anksagar-backend-sg.id
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.anksagar-backend-sg.id
   source_security_group_id = aws_security_group.anksagar-backend-sg.id
 }
