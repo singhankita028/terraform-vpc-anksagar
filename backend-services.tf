81bce393a3b35d603fc10cd87c7e48a9f016d98e
anksagar <ankita.a.sagar@capgemini.org>
2021-11-09 11:17:50 -0500

    bastion host

--- backend-services.tf
+++ backend-services.tf
@@ -34,7 +34,7 @@ resource "aws_elasticache_cluster" "anksagar-cache" {
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.4"
+  parameter_group_name = "default.memcached1.6"
   port                 = 11211
   security_group_ids   = [aws_security_group.anksagar-backend-sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.anksagar-ecache-subgrp.name
